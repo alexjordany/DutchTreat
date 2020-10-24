0189a1fa2ffb2ffacf2aa6cc66cee04d37c88da8
Alex Jordany Parrales López <alexjordany@outlook.com>
2020-10-23 20:09:43 -0600

    implementing and configuring identity

--- Dutch/Data/DutchSeeder.cs
+++ Dutch/Data/DutchSeeder.cs
@@ -1,5 +1,6 @@
 ﻿using DutchTreat.Data.Entities;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
@@ -13,16 +14,36 @@ namespace DutchTreat.Data
 	{
 		private readonly DutchContext _ctx;
 		private readonly IWebHostEnvironment _hosting;
+        private readonly UserManager<StoreUser> _userManager;
 
-		public DutchSeeder(DutchContext ctx, IWebHostEnvironment hosting)
+        public DutchSeeder(DutchContext ctx, IWebHostEnvironment hosting, UserManager<StoreUser> userManager)
 		{
 			_ctx = ctx;
 			_hosting = hosting;
-		}
+            _userManager = userManager;
+        }
 
-		public void Seed()
+		public async Task SeedAsync()
 		{
 			_ctx.Database.EnsureCreated();
+
+			StoreUser user = await _userManager.FindByEmailAsync("alexjordany@outlook.com");
+            if (user == null)
+            {
+				user = new StoreUser()
+				{
+					FirstName ="Alex",
+					LastName="Parrales",
+					Email ="alexjordany@outlook.com",
+					UserName="alexjordany@outlook.com"
+				};
+
+				var result = await _userManager.CreateAsync(user, "P@ssw0rd");
+				if(result != IdentityResult.Success)
+                {
+					throw new InvalidOperationException("Could not create new user in seeder");
+                }
+            }
 
 			if (!_ctx.Products.Any())
 			{
@@ -35,6 +56,7 @@ namespace DutchTreat.Data
 				var order = _ctx.Orders.Where(o => o.Id == 1).FirstOrDefault();
 				if (order != null)
 				{
+					order.User = user;
 					order.Items = new List<OrderItem>()
 					  {
 						new OrderItem()
